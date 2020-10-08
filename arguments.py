7c5047bd3023bb8633963e686bfde81297dc7fa9
Jeremiah Coholich <jcoholich@gatech.edu>
2020-10-08 01:21:56 -0400

    implemented policy network

--- arguments.py
+++ arguments.py
@@ -3,15 +3,23 @@ import argparse
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--num-env-steps', type=int, default=int(1e6), 
-        help='Total samples to train on')
+                        help='Total samples to train on')
     parser.add_argument('--num-steps', type=int, default=int(2048), 
-        help='Steps per update')
+                        help='Steps per update')
     parser.add_argument('--hidden-size', type=int, default=256, 
-        help='hidden size of the policy and value neural networks')
+                        help='hidden size of the policy and value neural networks')
     parser.add_argument('--hidden-layers', type=int, default=2, 
-        help='number of hidden layers in the policy and value neural networks')
+                        help='number of hidden layers in the policy and value neural networks')
     parser.add_argument('--activation', type=str, default='relu', 
-        help='non-linear activation function for hidden layers. (Choose from tanh, sigmoid, or relu)')
+                        help='non-linear activation function for hidden layers. (Choose from tanh, sigmoid, or relu)')
+    parser.add_argument('--seed', type=int, default=None,
+                        help='Random seed')
+    parser.add_argument('--deterministic', default=False, action='store_true',
+                        help='if True, actor will select mean of action distribution, instead of sampling')
+    parser.add_argument('--independent_std', default=False, action='store_true',
+                        help='if True, standard deviations of the selected actions will not depend on state')
+    # todo add option for output tanh and options for how to parameterized std
     args = parser.parse_args()
+
     
     return args
--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import numpy as np
 import torch
 from torch import nn
 from arguments import get_args
+import os
 
+# TODO 
 
 # Define replay buffer object
 class ReplayBuffer:
@@ -25,26 +27,81 @@ class ReplayBuffer:
         self.idx += 1
 
         if self.idx == self.N:
+            self.idx = 0
             return True # return value indicates timeout
         else:
             return False
 
 
     def store_terminal_obs(self, obs):
-        assert self.idx == self.N
-        self.obs[self.idx] = obs
+        self.obs[self.N] = torch.from_numpy(obs).float()
 
 
 class Policy(nn.Module):
-    def __init__(self, hidden_size=256, hidden_layers=2, activation='relu'):
+    def __init__(self, action_dim, obs_dim, hidden_size=256, hidden_layers=2, activation='relu', deterministic=False,
+                independent_std=False):
         super(Policy, self).__init__() 
-        
-        
 
-    
+        action_dim, = action_dim
+        obs_dim, = obs_dim
+        activation = activation.lower()
+        self.deterministic = deterministic
+        self.independent_std = independent_std
+
+        if activation == 'relu':
+            self.act = nn.ReLU()
+        elif activation == 'tanh':
+            self.act = nn.Tanh()
+        elif activation == 'sigmoid':
+            self.act = nn.Sigmoid()
+        else:
+            raise ValueError('\'%s\' is not a currently supported activation function' % activation)
+
+        self.linear_layers = [nn.Linear(obs_dim, hidden_size)]
+        for _ in range(hidden_layers - 1):
+            self.linear_layers.append(nn.Linear(hidden_size, hidden_size))
+        self.linear_layers.append(nn.Linear(hidden_size, action_dim))
+
+        if not deterministic: 
+            if independent_std:
+                log_std = -0.5 * torch.ones(action_dim).float()
+                self.log_std = nn.Parameter(log_std, requires_grad=True)
+            else:
+                self.linear_log_std_layer = nn.Linear(hidden_size, action_dim)
+
+
+    def forward(self, obs):
+        ''' '''
+
+        obs = torch.from_numpy(obs).float()
+        for i in range(len(self.linear_layers) -1):
+            obs = self.act(self.linear_layers[i](obs))
+
+        if self.deterministic or self.independent_std: 
+        # Just output the action means, no need to calculate standard deviations
+            return self.linear_layers[-1](obs)
+        else:
+            # calculate and return means, log stds
+            return self.linear_layers[-1](obs), self.linear_log_std_layer(obs)
+
 
     def get_action(self, obs):
-        return np.array([4 * (np.random.rand() - 0.5)])
+        if self.deterministic:
+            return self(obs)
+        elif self.independent_std:
+            mean = self(obs) 
+            dist = torch.distributions.multivariate_normal.MultivariateNormal(mean,
+                covariance_matrix=self.log_std.exp().diag())
+            return dist.sample()
+        else: 
+            mean, log_std = self(obs)
+            dist = torch.distributions.multivariate_normal.MultivariateNormal(mean, 
+                covariance_matrix=log_std.exp().diag())
+            return dist.sample().numpy()
+
+
+    def update(self):
+        pass
 
 
 
@@ -60,7 +117,13 @@ def main():
 
     env = gym.make('Pendulum-v0')
     memory = ReplayBuffer(args.num_steps, env.observation_space.high.shape, env.action_space.high.shape)
-    policy = Policy(hidden_size=args.hidden_size, hidden_layers=args.hidden_layers, activation=args.activation)
+    policy = Policy(hidden_size=args.hidden_size,
+                    hidden_layers=args.hidden_layers, 
+                    activation=args.activation,
+                    action_dim=env.action_space.high.shape,
+                    obs_dim=env.observation_space.high.shape,
+                    deterministic=args.deterministic,
+                    independent_std=args.independent_std)
 
 
     for i in range(num_updates):
@@ -86,6 +149,6 @@ if __name__ == '__main__':
     main()
 
 
-        
+
 
 
