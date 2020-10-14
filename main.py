fixed gae calculation

main.py
@@ -5,6 +5,7 @@ from torch import nn
 from arguments import get_args
 import os
 import wandb
+import sys
 
 from models import ValueNet, Policy
 from storage import ReplayBuffer
@@ -26,12 +27,38 @@ from utils import NormalizedEnv
 
 # things to fix
 # should I be normalizing or clipping my action outputs? 
-# no gradients seem to be flowing back to train std parameters, even with an entropy bonus
-# additionally, the last policy layer bias isn't training at all
+# maybe do an ablation analysis where I take away the normalization.
+# Look up RL sanity checks 
+# Hack the EnvNormalize wrapper to make an extremely simple env to test in
+# I know its not an optimziation problem, its somewhere else
+
+
+# New steps to do 
+# See if I can learn this simple env with the kostrikov implementation
+# See what the normalized values of this env are 
+# Ok but seriously what I need to do is comb through each file in order, line for line, checking for mistakes
+
+
+# Combing through, things to double check
+# - when I bootstrap the reward and store the final observation, make sure things are fine in either case. Make sure 
+# nothing in the buffers carries over from previous iterations. I know the consequence of making the final reward zero 
+# is fine. What about leaving the final observation zero? I would like to force the predicted value of the final observa
+# tion to be zero if the timeout was done. Ultimately, I don't think I should worry about it because its rare to timeout
+# and be done at the same time. Actually, I will let the value net calculate the value of the final obs bc sometimes 
+# the env itself just times out. 
+
+# Actually, not sure if I should bootstrap the final reward either...because my value function uses GAE lambda. 
+# Ok...see what kostrikov does.
+# He differentiates between true terminal states and timeout end states
+# Kostrikov's reward buffer has shape N, not N + 1 like mine. Also check in compute_gae
+# Perhaps start by ignoring the env timeout endstates -- just treat those like true terminal
+
+
+# MY GAE LAMBDA DOESNT TAKE INTO ACCOUNT 'DONE'
 
 def main():
     wandb.login()
-    wandb.init(project='ppo-setup2', monitor_gym=False)
+    wandb.init(project='ppo-setup3', monitor_gym=False)
     args = get_args()
     wandb.config.update(args)
     num_updates = -(-args.total_steps//args.steps_per_update) # ceil division to ensure total steps are met
@@ -42,14 +69,15 @@ def main():
     #                             video_callable=lambda i: False)
 
     env = NormalizedEnv(gym.make(args.env_name), gamma=args.gamma)
+    # env = gym.make(args.env_name) #  
     torch.manual_seed(args.seed)
     env.seed(args.seed)
     replay_buffer = ReplayBuffer(args.steps_per_update, env.observation_space.high.shape, env.action_space.high.shape)
-    policy = Policy(hidden_size=args.hidden_size,
+    policy = Policy(action_dim=env.action_space.high.shape,
+                    obs_dim=env.observation_space.high.shape,
+                    hidden_size=args.hidden_size,
                     hidden_layers=args.hidden_layers,
                     activation=args.activation,
-                    action_dim=env.action_space.high.shape,
-                    obs_dim=env.observation_space.high.shape,
                     deterministic=args.deterministic,
                     independent_std=args.independent_std)
 
@@ -83,6 +111,7 @@ def main():
             timeout = replay_buffer.store(obs, action, reward, done, info)
             obs = next_obs
             if timeout and done:
+                replay_buffer.bootstrap_reward(obs, value_net(obs).item()) #TODO think about whether this is right or no
                 break
             if timeout:
                 replay_buffer.bootstrap_reward(obs, value_net(obs).item())

storage.py
@@ -40,36 +40,35 @@ class ReplayBuffer:
 
 
     def bootstrap_reward(self, obs, rew):
-        '''Store the final state and get a bootstrapped final reward. (This method is only called if the final state
-        is not terminal. '''
-        assert not self.done[-1]
-        assert len(self.obs) == self.N + 1
+        '''Store the final state and get a bootstrapped final reward.'''
+        # assert not self.done[-1]
+        # assert len(self.obs) == self.N + 1
         self.obs[self.N] = torch.from_numpy(obs).float()
         self.rewards[-1] = rew
 
 
-    def compute_rewards_to_go(self):
-        '''Computes rewards-to-go'''
-        for i in reversed(range(self.N)):
-            self.rtg[i] = self.rewards[i] + self.done[i] * (self.rtg[i + 1] if i < self.N - 1 else self.rewards[i])
+    # def compute_rewards_to_go(self):
+    #     '''Computes rewards-to-go'''
+    #     for i in reversed(range(self.N)):
+    #         self.rtg[i] = self.rewards[i] + self.done[i] * (self.rtg[i + 1] if i < self.N - 1 else self.rewards[i])
     
 
-    def compute_empirical_values(self, gamma):
-        '''Computes empirical values of each state, based on sample data (not from value function). However, this is
-        using the bootstrapped final value from the value function.'''
-        for i in reversed(range(self.N)):
-            self.empirical_values[i] = self.rewards[i] + gamma * self.done[i] * (self.empirical_values[i + 1] if\
-                                        i < self.N - 1 else self.rewards[i])
+    # def compute_empirical_values(self, gamma):
+    #     '''Computes empirical values of each state, based on sample data (not from value function). However, this is
+    #     using the bootstrapped final value from the value function.'''
+    #     for i in reversed(range(self.N)):
+    #         self.empirical_values[i] = self.rewards[i] + gamma * self.done[i] * (self.empirical_values[i + 1] if\
+    #                                     i < self.N - 1 else self.rewards[i])
 
 
     def compute_gae(self, predicted_values, gamma, gae_lambda):
         ''' Computes generalized advantages estimates'''
         assert not (predicted_values == 0).all() # make sure that predicted values have actually been passed
-        deltas = self.rewards[:-1] + gamma * predicted_values[1:] - predicted_values[:-1]
+        deltas = self.rewards[:-1] + gamma * predicted_values[1:] * (~self.done) - predicted_values[:-1]
         assert len(deltas) == self.N
         deltas = torch.cat((deltas, torch.Tensor([0.])))
         for i in reversed(range(self.N)):
-            self.advantages[i] = deltas[i] + gamma * gae_lambda * deltas[i + 1]
+            self.advantages[i] = deltas[i] + gamma * gae_lambda * deltas[i + 1] * (not self.done[i])
 
         # self.advantages -= self.advantages.mean()
         # self.advantages /= self.advantages.std() + 1e-10