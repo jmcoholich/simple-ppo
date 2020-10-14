added all logic to handle timeout AND FIXED GAE CALCULATION ERROR

algs.py
@@ -42,24 +42,32 @@ class PPO:
 
         # do preprocessing
         with torch.no_grad():
-            predicted_values = self.value_net(self.replay_buffer.obs).squeeze()
-            self.replay_buffer.compute_gae(predicted_values, self.gamma, self.gae_lambda)
-            value_targets = self.replay_buffer.advantages + predicted_values[:-1]
+            old_predicted_values = self.value_net(self.replay_buffer.obs).squeeze()
+            self.replay_buffer.compute_gae(old_predicted_values, self.gamma, self.gae_lambda) # checked 
+            # below is fine, just don't use supp
+            value_targets = self.replay_buffer.advantages + old_predicted_values[:-1] 
             old_log_probs, _  = self.policy.get_log_probs(self.replay_buffer.obs[:-1], self.replay_buffer.actions, 
-                                                            compute_entropy=False)
-            old_predicted_values = self.value_net(self.replay_buffer.obs[:-1]).squeeze()
-            advantages = self.replay_buffer.advantages - self.replay_buffer.advantages.mean()
+                                                            compute_entropy=False) # again just don't use supp
+            # calculation of mean exludes the 'bootstrapped' steps at end of every episode 
+            N = len(self.replay_buffer.advantages)
+            bs_idcs = self.replay_buffer.done.nonzero() + 1
+            if N in bs_idcs:
+                assert bs_idcs[0,-1] == N
+                bs_idcs = bs_idcs[:,:-1] # get rid of that last value, since it's already gone
+            mean_ = (self.replay_buffer.advantages.sum() - self.replay_buffer.advantages[bs_idcs].sum())/\
+                    (N - len(bs_idcs[0]))
+            advantages = self.replay_buffer.advantages - mean_
             advantages /= self.replay_buffer.advantages.std() + 1e-9
+            # ensure that the advantage at any bootstrapped step is zero, so that nothing backprops through it
+            advantages[bs_idcs] = 0.0
 
         # train the neural networks
         for _ in range(self.epochs):
             self.optimizer.zero_grad()
-            ppo_clip_obj, entropy = self._compute_ppo_clip_obj(old_log_probs, advantages, 
+            ppo_clip_obj, entropy = self._compute_ppo_clip_obj(old_log_probs, advantages, bs_idcs,
                 compute_entropy=True) #(0.0 != self.entropy_coef)) # Always compute entropy just for logging 
-            value_loss = self._compute_value_loss(value_targets, old_predicted_values)
+            value_loss = self._compute_value_loss(value_targets, old_predicted_values[:-1], bs_idcs)
             loss = -ppo_clip_obj + self.value_loss_coef * value_loss - self.entropy_coef * entropy
-            # print(loss)
-            # print(loss)
             loss.backward()
             self.optimizer.step()
 
@@ -72,20 +80,25 @@ class PPO:
                 self.epoch_counter += 1
 
 
-    def _compute_ppo_clip_obj(self, old_log_probs, advantages, compute_entropy=True):
+    def _compute_ppo_clip_obj(self, old_log_probs, advantages, bs_idcs, compute_entropy=True):
         '''Combine with entropy calculation to avoid separate fwd pass for entropy'''
         log_probs, entropy = self.policy.get_log_probs(self.replay_buffer.obs[:-1], self.replay_buffer.actions,
             compute_entropy=compute_entropy)
         ratios = (log_probs - old_log_probs).exp()
+        # calculate mean of entropy without including bs steps
+        assert len(entropy) == 1
+        entropy_mean = (entropy.sum() - entropy[bs_idcs].sum())/(len(entropy[0]) - len(bs_idcs[0]))
         return torch.min(ratios * advantages, torch.clamp(ratios, 1 - self.eps, 1 + self.eps) * advantages).mean(),\
-            entropy.mean()
+            entropy_mean
 
 
-    def _compute_value_loss(self, value_targets, old_predicted_values):
+    def _compute_value_loss(self, value_targets, old_predicted_values, bs_idcs):
         '''Returns clipped version of MSE loss for value'''
         current_value_predictions = self.value_net(self.replay_buffer.obs[:-1]).squeeze()
         value_loss = (current_value_predictions - value_targets).pow(2)
         value_pred_clipped = old_predicted_values + torch.clamp(current_value_predictions - old_predicted_values,
             -self.eps, self.eps)
         clipped_value_loss  = (value_pred_clipped - value_targets).pow(2)
-        return 0.5 * torch.max(value_loss, clipped_value_loss).mean()
+        loss_ = torch.max(value_loss, clipped_value_loss)
+        assert len(loss_) != 1
+        return 0.5 * (loss_.sum() - loss_[bs_idcs].sum())/(len(loss_) - len(bs_idcs[0]))

storage.py
@@ -16,8 +16,8 @@ class ReplayBuffer:
         self.real_rewards = torch.zeros(N + 1, dtype=torch.float32)
 
         # Calculated/passed values
-        self.rtg = torch.zeros(N, dtype=torch.float32)
-        self.empirical_values = torch.zeros(N, dtype=torch.float32)
+        # self.rtg = torch.zeros(N, dtype=torch.float32)
+        # self.empirical_values = torch.zeros(N, dtype=torch.float32)
         self.advantages = torch.zeros(N, dtype=torch.float32)
         # self.predicted_values = torch.zeros(N + 1, dtype=torch.float32)
 
@@ -50,12 +50,12 @@ class ReplayBuffer:
             return False
 
 
-    def bootstrap_reward(self, obs, rew):
-        '''Store the final state and get a bootstrapped final reward.'''
-        # assert not self.done[-1]
-        # assert len(self.obs) == self.N + 1
-        self.obs[self.N] = torch.from_numpy(obs).float()
-        self.rewards[-1] = rew
+    # def bootstrap_reward(self, obs, rew):
+    #     '''Store the final state and get a bootstrapped final reward.'''
+    #     # assert not self.done[-1]
+    #     # assert len(self.obs) == self.N + 1
+    #     self.obs[self.N] = torch.from_numpy(obs).float()
+    #     self.rewards[-1] = rew
 
 
     # def compute_rewards_to_go(self):
@@ -75,11 +75,12 @@ class ReplayBuffer:
     def compute_gae(self, predicted_values, gamma, gae_lambda):
         ''' Computes generalized advantages estimates'''
         assert not (predicted_values == 0).all() # make sure that predicted values have actually been passed
-        deltas = self.rewards[:-1] + gamma * predicted_values[1:] * (~self.done) - predicted_values[:-1]
+        deltas = self.rewards[:-1] + gamma * predicted_values[1:] * (~(self.done and (~ self.timeout)))\
+                 - predicted_values[:-1]
         assert len(deltas) == self.N
         deltas = torch.cat((deltas, torch.Tensor([0.])))
         for i in reversed(range(self.N)):
-            self.advantages[i] = deltas[i] + gamma * gae_lambda * deltas[i + 1] * (not self.done[i])
+            self.advantages[i] = deltas[i] + gamma * gae_lambda * self.advantages[i + 1] * (not self.done[i])
 
         # self.advantages -= self.advantages.mean()
         # self.advantages /= self.advantages.std() + 1e-10